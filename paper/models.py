d1079b504f974e081a0dd180844712f9189b2a76
antiver <antiver@foxmail.com>
2019-07-08 14:22:40 +0800

    添加了paper的todo list

--- paper/models.py
+++ paper/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Paper(models.Model):
+    # TODO 论文的一些属性
+    ...
+
+
+class Comment(models.Model):
+    # TODO 用户对论文的评论
+    ...
--- paper/views.py
+++ paper/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    # TODO 文献检索库的主界面
+    ...
+
+
+def upload(request):
+    # TODO 文献上传页面
+    ...
