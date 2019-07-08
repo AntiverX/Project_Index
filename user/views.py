420a034b88607ec436bf39411855902de5cd482e
antiver <antiver@foxmail.com>
2019-07-08 14:11:40 +0800

    添加了user的todo list 添加了gitignore

--- user/views.py
+++ user/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def register(request):
+    # TODO 注册界面
+    ...
+
+
+def login(request):
+    # TODO 登录界面
+    ...
+
+
+def index(request):
+    # TODO 显示用户自己的相关信息
+    ...
